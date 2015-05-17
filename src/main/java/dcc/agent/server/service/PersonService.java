2a98fcf2214dd161211a8f920957bb4a27678df5
tchambill <antony_epis@hotmail.com>
2015-05-16 23:43:11 -0400

    Initial commit

---
+++ src/main/java/dcc/agent/server/service/PersonService.java
@@ -0,0 +1,43 @@
+package dcc.agent.server.service;
+
+import dcc.agent.server.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by teo on 16/05/15.
+ */
+
+@Repository
+public class PersonService {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    public static final String COLLECTION_NAME = "person";
+
+    public void addPerson(Person person) {
+        if (!mongoTemplate.collectionExists(Person.class)) {
+            mongoTemplate.createCollection(Person.class);
+        }
+        person.setId(UUID.randomUUID().toString());
+        mongoTemplate.insert(person, COLLECTION_NAME);
+    }
+
+    public List<Person> listPerson() {
+        return mongoTemplate.findAll(Person.class, COLLECTION_NAME);
+    }
+
+    public void deletePerson(Person person) {
+        mongoTemplate.remove(person, COLLECTION_NAME);
+    }
+
+    public void updatePerson(Person person) {
+        mongoTemplate.insert(person, COLLECTION_NAME);
+    }
+}
+
